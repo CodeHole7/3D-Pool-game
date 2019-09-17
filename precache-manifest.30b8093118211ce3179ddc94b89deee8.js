df4ac4e20ab268b434ef342e134a32089a2d2dc7
Anna <annenkovaa905@gmail.com>
2019-09-17 13:04:46 -0500

    Initial commit

---
+++ precache-manifest.30b8093118211ce3179ddc94b89deee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb2fc4aa478a406831062a504fc3a91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "575e8173e751dc2fc529",
+    "url": "/static/css/main.459a737e.chunk.css"
+  },
+  {
+    "revision": "bbae58a57601158c9c19",
+    "url": "/static/js/2.889d2fa6.chunk.js"
+  },
+  {
+    "revision": "575e8173e751dc2fc529",
+    "url": "/static/js/main.ba521f85.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "62cee232d3b161c24323c98179140c4e",
+    "url": "/static/media/cueball.62cee232.png"
+  }
+]);
---
+++ readme.md
@@ -0,0 +1,2 @@
+# React.js-3D-Pool-game
+ React.js Babylon.js WebGL project
